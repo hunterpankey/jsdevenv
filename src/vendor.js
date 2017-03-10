5676d7325e0ec823e42d2dc80886a3d77d921c57
Hunter <hunterpankey@gmail.com>
2017-03-10 10:05:59 -0500

    Project structure, build, and deployment

---
+++ src/vendor.js
@@ -0,0 +1,8 @@
+/** Vendor library references
+ * used by webpack for the production build only
+ * Anything not referenced here will be bundled in the application JavaScript bundle.
+ */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
